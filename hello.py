100aec194638634533e6d62c9b0e14823aafebcc
DemiënFeenstra <99064788@mydavinci.nl>
2021-10-27 11:19:33 +0200

    lab 3 gemaakt

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("hello world")
+def Myfunction():
+    print("pppp    y     y   ttttttt   h   h    oooo   nnnn ")
+    print("p   p    y   y       t      h   h   o    o  n   n")
+    print("p   p     y y        t      h   h   o    o  n   n")
+    print("pppp       y         t      hhhhh   o    o  n   n")
+    print("pppp       y         t      hhhhh   o    o  n   n")
+    print("p          y         t      h   h   o    o  n   n")
+    print("p          y         t      h   h    oooo   n   n")
+Myfunction()
+
+# Demiën Feenstra 99064788
