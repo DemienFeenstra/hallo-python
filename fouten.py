ee0ae046c60b27476ec6ca503dcf12dc2a936f25
DemiënFeenstra <99064788@mydavinci.nl>
2021-10-29 10:01:40 +0200

    lab 5 af

---
+++ fouten.py
@@ -0,0 +1,14 @@
+# fout1
+# print(hello world)
+
+# fout2
+# print("hello world")
+
+# fout3
+# print "hello world"
+
+# fout4
+# input(name)
+
+# fout5
+
---
+++ lab5.py
@@ -0,0 +1,47 @@
+# Opdracht 1
+# het totaal bedrag aan collegegeld betaald door (de ouders van) alle studenten uit jouw klas voor het huidige studiejaar.
+# Hoeveel studenten zijn er in jouw klas?
+# Wat is het collegegeld voor dit studiejaar?
+# Even opzoeken. En vraag jezelf af: Vanaf welke leeftijd wordt collegegeld gevraagd?
+
+studenten = 17
+collegegeld = 542
+totaal = studenten * collegegeld
+print("het totaal bedrag aan collegegeld is:" + " " + str(totaal))
+
+
+
+
+
+
+
+
+
+
+# Opdracht 2
+# de BTW voor al het fruit in de fruitmand:
+#  je betaalde 3.40 euro voor de appels, 2.45 voor de druiven, 1.95 voor de bananen. En By The Way wat is de BTW op fruit? Even opzoeken!
+# 9% btw op fruit
+
+appels = 3.40
+druiven = 2.45
+bananen = 1.95
+btw = 0.702
+totaal = appels + druiven + bananen
+
+print(totaal + btw)
+
+# Opdracht 3
+# Of verzin zelf een paar interessante sommen!
+
+x = 7
+print(x * 1)
+print(x * 2)
+print(x * 3)
+print(x * 4)
+print(x * 5)
+print(x * 6)
+print(x * 7)
+print(x * 8)
+print(x * 9)
+print(x * 10)
